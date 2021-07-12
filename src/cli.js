6adbd09b1de1a2a81ebd03008e8e06337e893406
Elizaveta <matronka@mail.ru>
2021-07-12 23:22:05 +0300

    Step 4

--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,6 @@
 import readlineSync from 'readline-sync';
 
-var name = readlineSync.question(`Welcome to the Brain Games!
+const name = readlineSync.question(`Welcome to the Brain Games!
 May I have your name? `);
 
-export { name };
+export default name;
